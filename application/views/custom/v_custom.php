a884480af105af9ac40680d144c249f8adfbd4a2
edo pratama <kimbisnis3@gmail.com>
2019-10-08 14:48:50 +0700

    update

--- application/views/custom/v_custom.php
+++ application/views/custom/v_custom.php
@@ -16,12 +16,27 @@
     <div class="konten">
         <div class="container">
             <!-- <iframe src="<?php echo prep_url('http://demo.gongsoft.com/plugin/customdesign/editor.php?product=10') ?>" height="600px" width="100%" style="margin-top : 25px !important;"></iframe> -->
-            <iframe src="<?php echo prep_url('http://localhost/lumise_custom/editor.php?product=3') ?>" height="600px" width="100%" style="margin-top : 25px !important;"></iframe>
+            <iframe id="iframe" src="<?php echo prep_url('http://localhost/lumise_custom/editor.php?product=3') ?>" height="600px" width="100%" style="margin-top : 25px !important;"></iframe>
         </div>
     </div>
     <?php $this->load->view('_partials/foot.php'); ?>
 </body>
 <script>
-    
+    $(function(){
+			var f=$('#iframe')
+			f.load(function(){ 
+                f.contents().find(`
+                    name,
+                    price , 
+                    desc,
+                    #lumise-change-product,
+                    #lumise-top-tools,
+                    .how-calculate,
+                    .lumise-prints,
+                    [data-tab="bug"],
+                    [data-type="quantity"]
+                `).remove()
+			})
+		})
 </script>
 </html>
